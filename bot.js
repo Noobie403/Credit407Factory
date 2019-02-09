714811f67d1eb683ce438bc27717fbabd91d66ae
Noobie403 <47475293+Noobie403@users.noreply.github.com>
2019-02-09 21:55:47 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("543752674055553029")
+setInterval(function() {
+channel.send(`hello mother father i need you mam to fuck him in the ass but it يا ورع كلزق سوي فعالية لاشخ بطيزك واحشي زبي بطيزك`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
